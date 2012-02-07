e41af4c850e80d411be740d297cd7e9c8b6d254e
Michael Barton <mail@michaelbarton.me.uk>
2012-02-07 15:30:14 -0500

    Script to generate gff form csv data

---
+++ annotations/create_gff.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+require 'csv'
+require 'bio'
+
+raw       = File.join(File.dirname(__FILE__),'gene_list.csv')
+name_file = File.join(File.dirname(__FILE__),'names.yml')
+output    = File.join(File.dirname(__FILE__),'..','assembly','annotations.gff')
+
+names = YAML.load(File.read(name_file))
+
+gffs = CSV.read(raw).map do |line|
+  id, start, stop, strand, gc, _, _, product, _ = line
+  name = names[id.to_i]
+
+  attr = {'ID' => id, 'note' => product}
+  attr['Name'] = name if name
+
+  Bio::GFF::GFF3::Record.new('scaffold00008',nil,'gene',start,stop,nil,strand,nil,attr)
+end
+
+File.open(output,'w') do |out|
+  out << "##gff-version 3\n"
+  gffs.each do |gff|
+    out << gff.to_s
+  end
+end
